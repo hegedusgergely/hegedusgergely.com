e78dcdb6982d1ff43ba9f3fdfa2124417810d30e
Hegedűs Gergely <mail@hegedusgergely.com>
2016-11-15 20:04:34 +0100

    initial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Basic site preferences -->
+        <meta charset="utf-8">
+        <title>Hegedűs Gergely</title>
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:700i|Raleway:300,300i&subset=latin-ext">
+        <!-- Mobile view helper -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- These all for favicons -->
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ URL::asset('favico/apple-touch-icon-57x57.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::asset('favico/apple-touch-icon-114x114.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::asset('favico/apple-touch-icon-72x72.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::asset('favico/apple-touch-icon-144x144.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ URL::asset('favico/apple-touch-icon-60x60.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ URL::asset('favico/apple-touch-icon-120x120.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ URL::asset('favico/apple-touch-icon-76x76.png') }}"/>
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ URL::asset('favico/apple-touch-icon-152x152.png') }}"/>
+        <link rel="icon" type="image/png" href="{{ URL::asset('favico/favicon-196x196.png" sizes="196x196') }}"/>
+        <link rel="icon" type="image/png" href="{{ URL::asset('favico/favicon-96x96.png" sizes="96x96') }}"/>
+        <link rel="icon" type="image/png" href="{{ URL::asset('favico/favicon-32x32.png" sizes="32x32') }}"/>
+        <link rel="icon" type="image/png" href="{{ URL::asset('favico/favicon-16x16.png" sizes="16x16') }}"/>
+        <link rel="icon" type="image/png" href="{{ URL::asset('favico/favicon-128.png" sizes="128x128') }}"/>
+        <meta name="application-name" content="&nbsp;"/>
+        <meta name="msapplication-TileColor" content="#F1EDF8" />
+        <meta name="msapplication-TileImage" content="{{ URL::asset('favico/mstile-144x144.png') }}" />
+        <meta name="msapplication-square70x70logo" content="{{ URL::asset('favico/mstile-70x70.png') }}" />
+        <meta name="msapplication-square150x150logo" content="{{ URL::asset('favico/mstile-150x150.png') }}" />
+        <meta name="msapplication-wide310x150logo" content="{{ URL::asset('favico/mstile-310x150.png') }}" />
+        <meta name="msapplication-square310x310logo" content="{{ URL::asset('favico/mstile-310x310.png') }}" />
+    </head>
+    <body>
+
+        <header>
+            <div class="container">
+                <div class="row">
+                    <h1>Hegedűs Gergely</h1>
+                </div>
+            </div>
+        </header>
+
+        <section id="about">
+            <div class="container">
+                <div class="row">
+                    <p>
+                        Visual person without compromises.<br>
+                        Dreamer with an innovative spirit.<br>
+                        Tech and coffee lover.<br>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section id="social">
+            <div class="container">
+                <div class="row">
+                    <p>
+                        You can find me on <a class="hvr-underline-from-left" href="https://twitter.com/hegedusgergely" target="_blank">Twitter</a> and
+                        <a class="hvr-underline-from-left" href="https://www.instagram.com/hegedus.geri/" target="_blank">Instagram</a> or
+                        <a class="hvr-underline-from-left" href="mailto:mail@hegedusgergely.com?Subject=Hey%20There" target="_top">You can E-mail me.</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
